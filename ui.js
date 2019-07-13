0f4ff3cc1a4ae4c00d4e2e47ebb734fb36ba158e
Kira <kira.mueller@outlook.com>
2019-07-13 11:45:33 -0700

    API call working

--- ui.js
+++ ui.js
@@ -2,16 +2,16 @@ var wind = {
     speedMax: 0,
     speedMin: 0,
     direction: "N"
-}
+};
 
-var location = {
+var locationProp = {
     name: "",
     lat: 0.00,
     long: 0.00,
     address: ""
-}
+};
 
-var locationResults = [ /* INTENT: ARRAY OF LOCATIONS */ ];
+var locationResults = [];
 
 var requestFamilyFriendly = false;
 var requestDate = Date.now();
--- weather.js
+++ weather.js
@@ -2,17 +2,42 @@ var weather = {
     //takes a lattitude and longitude and returns an object with wind speed and gust speed
 
 
-    function getWeather (lat,long) {
-        var key = 44d4a55b5025e5a21a8ab11202df6b6c;
-        var URL = `https://api.darksky.net/forecast/{key}/{lat},{long}`;
+    getWeather: function (lat,long) {
+        var key = '44d4a55b5025e5a21a8ab11202df6b6c';
+        var url = 'https://api.darksky.net/forecast/' + key + '/' + lat + ',' + long;
+        var speed,gust,dir;
 
-        $.get(URL,function(response) {
-            
+        $.ajax({
+            url:url,
+            method:'GET'
+        }).then(function(response){
+            console.log(JSON.stringify(response));
+            var speed,gust,dir;
+
+            speed = response.currently.windSpeed;
+            gust = response.currently.windGust;
+            dir = response.currently.windBearing;
         });
-    }
+
+
+
+        // return {speedMax:gust,speedMin:speed,direction:dir};
+
+        // $.get(URL,function(response) {
+        //     var speed,gust,dir;
+
+        //     speed = response.currently.windSpeed;
+        //     gust = response.currently.windGust;
+        //     dir = response.currently.windBearing;
+
+        //     return {speedMax:gust,speedMin:speed,direction:dir};
+        // });
+    },
 
     //takes an array of place objects, an object with requirements, and a number of requested results, returns an array of place objects
-    function topSpots(places, requirements, numRequested) {
+    topSpots: function(places, requirements, numRequested) {
 
     }
 };
+
+console.log(weather.getWeather(37.8267,-122.4233));
