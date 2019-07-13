66298a1219e5817d907f4c02394954e67bb2f5c1
sharryheb <43225557+sharryheb@users.noreply.github.com>
2019-07-13 10:46:46 -0700

    reorg directory structure, fix location object name

--- javascript/ui.js
+++ javascript/ui.js
@@ -1,19 +1,20 @@
+
 var wind = {
     speedMax: 0,
     speedMin: 0,
     direction: "N"
-}
+};
 
-var location = {
+var locationProp = {
     name: "",
-    lat: 0.00,
-    long: 0.00,
+    lat: 0,
+    long: 0,
     address: ""
-}
+};
 
-var locationResults = [ /* INTENT: ARRAY OF LOCATIONS */ ];
+var locationResults = []; 
 
 var requestFamilyFriendly = false;
-var requestDate = Date.now();
+var requestDate = new Date();
 var requestRadius = 0;
 var unitsImperial = true;
