7c8c8d0b696598b4abc0bf976ac8cfab4be37178
Kira <kira.mueller@outlook.com>
2019-07-15 20:04:07 -0700

    topSpots calls function to render pins on map

--- javascript/weather.js
+++ javascript/weather.js
@@ -1,6 +1,13 @@
-
-
-
+/*
+place object format:
+{
+    lat:12.34,
+    long:12.34,
+    speedMax:15,
+    speedMin:10,
+    direction:234
+}
+*/
 var weather = {
     //takes a lattitude and longitude and returns an object with wind speed and gust speed
 
@@ -9,37 +16,21 @@ var weather = {
         var key = '44d4a55b5025e5a21a8ab11202df6b6c';
         var url = `https://api.darksky.net/forecast/${key}/${place.lat},${place.long}`;
 
-        $.ajax({
+        return $.ajax({
             url:url,
             method:'GET'
-        }).then(function(response){
-            // console.log(JSON.stringify(response));
-
-            place.speed = response.currently.windSpeed;
-            place.gust = response.currently.windGust;
-            place.dir = response.currently.windBearing;
         });
-
-        console.log(place);
 
     },
 
-    //takes an array of place objects, an object with requirements, and a number of requested results, returns an array of place objects
-    topSpots: function(places) {
-        // for (let i = 0; i < places.length; i++) {
-        //     let p = places[i];
-        //     this.getWeather(p).then(response => {
-        //         p.speedMax = response.currently.windGust;
-        //         p.speedMin = response.currently.windSpeed;
-        //         p.direction = response.currently.windBearing;
-        //     });
-        // }
-
+    //takes an array of place objects, an object with requirements, returns an array of place objects
+    topSpots: function(places,min,max) {
 
         let requests = places.map(this.getWeather);
 
         Promise.all(requests)
             .then(responses => {
+                var bestPlaces = [];
                 for (let i =0; i < responses.length; i++) {
                     let p = places[i];
                     let r = responses[i];
@@ -48,11 +39,14 @@ var weather = {
                     p.speedMin = r.currently.windSpeed;
                     p.direction = r.currently.windBearing;
 
-                    console.log(p);
+                    if(p.speedMax <== max && p.speedMin >== min) {
+                        bestPlaces.push(p);
+                    }
+
+                    // console.log(p);
                 }
-
                 
-
+                markPlaces(bestPlaces);
             });
 
 
@@ -60,8 +54,7 @@ var weather = {
 };
 
 
-var parks = [{lat:34,long:45,speedMax:null,speedMin:null,direction:null},{lat:29,long:93,speedMax:null,speedMin:null,direction:null}];
+// var parks = [{lat:34,long:45,speedMax:null,speedMin:null,direction:null},{lat:29,long:93,speedMax:null,speedMin:null,direction:null}];
 
-weather.getWeather(parks[0]);
 
 
