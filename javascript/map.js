4f54ca888352c736e5d629bc1d2657efd2b0029f
U-DESKTOP-HI8IHO4\Thomas <thomasrodgers271@gmail.com>
2019-07-15 21:30:15 -0700

    made foursquare fill parks

--- javascript/map.js
+++ javascript/map.js
@@ -1,5 +1,4 @@
 
-
 var map;
 var vectorSource;
 var parks;
@@ -50,106 +49,23 @@ function createMap(position) {
 
 
 
-  fetch('https://api.foursquare.com/v2/venues/explore?client_id='
-    + fourSquare.clientId + '&client_secret=' + fourSquare.clientSecret +
-    '&limit=50&ll=' + String([lonLat[1], lonLat[0]]) + '&query=park')
+  $.ajax({
+    url: 'https://api.foursquare.com/v2/venues/search?client_id='
+      + fourSquare.clientId + '&client_secret=' + fourSquare.clientSecret +
+      '&v=20190715&limit=50&ll=' + String([lonLat[1], lonLat[0]]) + '&query=park',
+    method: 'GET'
+  })
     .then(function (response) {
       // Code for handling API response
-      console.log(response);
-    })
-    .catch(function (error) {
-      // Code for handling errors
-      console.log(error);
-    });
-}
-
-
-
-function addMapMarker(lonLat) {
-  // Add a marker to the map, takes longitude and latitude as an array.
-  var marker = new ol.Feature({
-    geometry: new ol.geom.Point(
-      ol.proj.fromLonLat(lonLat)
-    )
-  });
-  marker.setStyle(iconStyle);
-  vectorSource.addFeature(marker);
-}
-
-function markPlaces(places) {
-  for (var i = 0; i < places.length; ++i) {
-    addMapMarker([places[i].long, places[i].lat]);
-  }
-}
-
-
-
-if ('geolocation' in navigator)
-  navigator.geolocation.getCurrentPosition(createMap, createMap);
-else
-  createMap();
-
-
-
-
-
-
-var map;
-var vectorSource;
-var parks;
-
-var iconStyle = new ol.style.Style({
-  image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
-    anchor: [37, 40],
-    anchorXUnits: 'pixels',
-    anchorYUnits: 'pixels',
-    opacity: 1.0,
-    src: 'images/kite-icon.png'
-  }))
-});
-
-function createMap(position) {
-  // Create the map with given position from geopositioning.
-  console.log('Creating Map');
-  var lonLat;
-  if (position && position.coords) {
-    lonLat = [position.coords.longitude, position.coords.latitude];
-  } else {
-    lonLat = [-122.190933, 47.613701];
-  }
-  map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat(lonLat),
-      zoom: 12
-    })
-  });
-  var marker = new ol.Feature({
-    geometry: new ol.geom.Point(
-      ol.proj.fromLonLat(lonLat)
-    )
-  });
-  vectorSource = new ol.source.Vector({
-    features: [marker]
-  });
-  var markerVectorLayer = new ol.layer.Vector({
-    source: vectorSource,
-  });
-  map.addLayer(markerVectorLayer);
-
-
-
-  fetch('https://api.foursquare.com/v2/venues/explore?client_id='
-    + fourSquare.clientId + '&client_secret=' + fourSquare.clientSecret +
-    '&limit=50&ll=' + String([lonLat[1], lonLat[0]]) + '&query=park')
-    .then(function (response) {
-      // Code for handling API response
-      console.log(response);
+      var venues = response.response.venues;
+      for (var i = 0; i < venues.length; ++i) {
+        console.log(venues[i]);
+        parks.push({
+          name: venues[i].name,
+          lat: venues[i].location.latitude,
+          long: venues[i].location.longitude
+        });
+      }
     })
     .catch(function (error) {
       // Code for handling errors
