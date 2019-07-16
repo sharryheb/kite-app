44c9fb812d577402bbf5f5082b5701b99b024390
Kira <kira.mueller@outlook.com>
2019-07-15 18:23:00 -0700

    multiple API calls working

--- weather.js
+++ weather.js
@@ -1,43 +1,67 @@
+
+
+
 var weather = {
     //takes a lattitude and longitude and returns an object with wind speed and gust speed
 
 
-    getWeather: function (lat,long) {
+    getWeather: function (place) {
         var key = '44d4a55b5025e5a21a8ab11202df6b6c';
-        var url = 'https://api.darksky.net/forecast/' + key + '/' + lat + ',' + long;
-        var speed,gust,dir;
+        var url = `https://api.darksky.net/forecast/${key}/${place.lat},${place.long}`;
 
         $.ajax({
             url:url,
             method:'GET'
         }).then(function(response){
-            console.log(JSON.stringify(response));
-            var speed,gust,dir;
+            // console.log(JSON.stringify(response));
 
-            speed = response.currently.windSpeed;
-            gust = response.currently.windGust;
-            dir = response.currently.windBearing;
+            place.speed = response.currently.windSpeed;
+            place.gust = response.currently.windGust;
+            place.dir = response.currently.windBearing;
         });
 
+        console.log(place);
 
-
-        // return {speedMax:gust,speedMin:speed,direction:dir};
-
-        // $.get(URL,function(response) {
-        //     var speed,gust,dir;
-
-        //     speed = response.currently.windSpeed;
-        //     gust = response.currently.windGust;
-        //     dir = response.currently.windBearing;
-
-        //     return {speedMax:gust,speedMin:speed,direction:dir};
-        // });
     },
 
     //takes an array of place objects, an object with requirements, and a number of requested results, returns an array of place objects
-    topSpots: function(places, requirements, numRequested) {
+    topSpots: function(places) {
+        // for (let i = 0; i < places.length; i++) {
+        //     let p = places[i];
+        //     this.getWeather(p).then(response => {
+        //         p.speedMax = response.currently.windGust;
+        //         p.speedMin = response.currently.windSpeed;
+        //         p.direction = response.currently.windBearing;
+        //     });
+        // }
+
+
+        let requests = places.map(this.getWeather);
+
+        Promise.all(requests)
+            .then(responses => {
+                for (let i =0; i < responses.length; i++) {
+                    let p = places[i];
+                    let r = responses[i];
+
+                    p.speedMax = r.currently.windGust;
+                    p.speedMin = r.currently.windSpeed;
+                    p.direction = r.currently.windBearing;
+
+                    console.log(p);
+                }
+
+                
+
+            });
+
 
     }
 };
 
-console.log(weather.getWeather(37.8267,-122.4233));
+
+var parks = [{lat:34,long:45,speedMax:null,speedMin:null,direction:null},{lat:29,long:93,speedMax:null,speedMin:null,direction:null}];
+
+weather.getWeather(parks[0]);
+
+
