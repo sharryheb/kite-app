6e89b67e1bea72a45807b365c1200a28e7359b50
U-DESKTOP-HI8IHO4\Thomas <thomasrodgers271@gmail.com>
2019-07-20 11:29:00 -0700

    even more api keys

--- javascript/secrets.js
+++ javascript/secrets.js
@@ -13,16 +13,18 @@ var gibberish = {
   },
   'darkSky': {
     'keys': [
-      'U2FsdGVkX1+hJDYzxtEsssHSBksllWgS9DwOpRdaEg3mVL3RIHgcsv2miaoigirzqt8iyRA3qCzz8YQ9ZFMv3g==',
-      'U2FsdGVkX19Ci/Q7oCSFBdrLdbvcvlWtU4QgOIjVq7zxGrDTghuDONblS3U/G+20QYUgvMyjTvSGP8IRp+H34w==',
-      'U2FsdGVkX1/90mPo2TFJ0CEMs8iFX7h+qDrVXeu863NzNpAdmc+05SmT0b0Jx8zsvPPIt6jAMX/Wa4xpQmuHag==',
-      'U2FsdGVkX1/OsuaHqxaHuyeEOJPaHl/o3bd2vRTKg2c8g78Qz4wH865nN4PwIOTVgHSm3i0gy8tNyCKZLIv4ZA==',
-      'U2FsdGVkX18fRSs0VERxVS2ISlcEIe8wI44B330vqsvOS/0bh3N7V8GTk+P2gdqE4W5Ohub3vr8PdffRFxUOeg==',
-      'U2FsdGVkX19P7kPLMKO8HpU9S6zUMiJFMIih82W8er2Pplw4cMTLhEMcKirRx3PykeIBtNOYvIBZVzJ6AWV2qQ==',
-      'U2FsdGVkX1/W3BAgtjzX/jqF3woE4zeKfbhygV0JL2LE7QMqflfmuA2C3adLCZiwoyKBSmG6QZaPYF9oszlmuA==',
-      'U2FsdGVkX18uv8MbdQ590rIFOBTwO9W97Mk3fczu5z3EZCcT5hEB0T8m7VgTs6408O0GWb4bk8YEVFNup2vGRA==',
-      'U2FsdGVkX1+0MJmcQxxt11Ba78GLKlMBw/lGyvaxneSUhNUHJye6C8IsL8y7rXOKVzUo+k1e74vJTCti/KXRlg==',
-      'U2FsdGVkX19eyf3O5TqPLDrk2rtlFOlcy7LlAp6ZE8/QlPo+Wkv4KOJ5mCAzEJXaWBff2JNbjBg5vwDhjUi26Q=='
+      'U2FsdGVkX18vympKWCbgABjuY3YM5m9HwsQGBwkQKwRBx5rgezLnlGXit+ah6hOHIf9yoVG166NTjNRTyE+T8g==',
+      'U2FsdGVkX19DHosg6dYIoOEYZhdYLPZAJJnws/5etAumcK89Z4vEPS+LGSXld51NVE5JDDCja6oRKEwOlqZh0A==',
+      'U2FsdGVkX183/MFYG+5Y9YTgWMJXTfsBxqKT4zKX/Dlk0TW6aOspLwQaknW1EMYlZxFvT3eyKrSKY8Dt2pZx7Q==',
+      'U2FsdGVkX19PXvYfuEzn6e2h6/C9+vfZ7qEToGN/AQ9Hv4aowXIUrDHV97rvKk576go7LNJrqHiBmQ2qlAfBqQ==',
+      'U2FsdGVkX19VxvY7qbFLWem9n64HdPjW2kj+pVy9td7m02OmUPX/KQ6VQhSxkw51oFhSwDZPuV+T+SlfdE08zw==',
+      'U2FsdGVkX19xzMnu9/fd1qm9dxmgXgQ+lLq/x5Nlri8nqdrBlqxLeoDOH2Dkagj+cWzGp7cDwc3tHNqYcfUSCg==',
+      'U2FsdGVkX1992Ux7b5xxbKIX0yHGhGoUhxaOo5OZZYAwETnsBLf1DbcJnfqXr+FG7eOCrorXTdbPAOnPYpEy5w==',
+      'U2FsdGVkX1+z0hIhG6AKoFgNm8eWHGWlv20C3ZkPWCgJaPVhxKjn2ZJyPTrxzxtXSTwvzw486eisg8RbHkq91A==',
+      'U2FsdGVkX18XJT5gi1dXFNDGUDzFoTr+nLANJ22l3v3ui4/KtbcMCjhuWYJL7g2Q8OJDBAWg0An/WysOPGQPqA==',
+      'U2FsdGVkX19EQhF0L3Z7GNy2fttfR5UAr6mNzMm8Q6IusnAq1WFYJig4UFh25ro3zEm4QZj3Hk8cOqljinXMlw==',
+      'U2FsdGVkX1+y4TsFEMHNuhPy6tihXKRn6UzKFFwawmvxsI8OSRSu7frPiWLjEZtOOjhMtzDTEpFbM7AKcs7hSg==',
+      'U2FsdGVkX1/YtArgvKkZWxjLBsHdAOAhQO658SRCPM9eyKvGJWBzC2i5gpiDmE1TWy6wfHjBWat+vvFSjon1sQ=='
     ],
     i: 0
   }
